2cd20fc03b9ae6f97f88554f2c4b31e9fb78e7c4
Jashele T <jashelet@Jashele.domain>
2019-05-24 20:34:36 -0700

    updated closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const lastName = 'Tillman';
+
+function greet() {
+  const firstName = 'Jashele';
+  alert(`The names ${Tillman}, ${Jashele}`);
+}
+
+console.log(lastName)
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
